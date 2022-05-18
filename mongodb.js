d3e5a0b588a2ca0219a863e299d592a32c31458a
foxcatcher24 <tmitul515@gmail.com>
2022-05-18 17:52:42 +0600

    180522-- inserting into mongo

--- mongodb.js
+++ mongodb.js
@@ -2,29 +2,34 @@ const { MongoClient } = require("mongodb");
 
 
 // Connection URI
-const connectionUri ="mongodb://127.0.0.1:27017";
+const connectionUri = 'mongodb://localhost:27017';
 const databaseName = 'task-manager';
 
 // Create a new MongoClient
 const client = new MongoClient( connectionUri );
 
-async function main() {
+async function run() {
     // Use connect method to connect to the server
     await client.connect();
-    
+
     console.log('Connected successfully to server');
-    
+
     const db = client.db( databaseName );
+
     const collection = db.collection('users').insertOne({
-        "name":"tusker",
-        "age":30
+        'name': 'tusker',
+        'age' : 30
+    },( error, result) => {
+        if( error ){
+            return console.log('Unable to insert user');
+        }
+
+        console.log( result.acknowledged );
     });
 
     // the following code examples can be pasted here...
     return 'done.';
 }
 
-main()
-    .then( console.log )
-    .catch( console.error )
-    .finally( () => client.close() );
+run().then( console.log )
+    .catch( "Unable to insert data" );
