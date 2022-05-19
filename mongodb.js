d2b696e0f6606c46825777a7a831fa261aefe89e
foxcatcher24 <tmitul515@gmail.com>
2022-05-19 23:53:31 +0600

    190522--The ObjectId in mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,15 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 
 // Connection URI
 const connectionUri = 'mongodb://localhost:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectId();
+console.log( id );
+// console.log( id.id.length );
+// console.log( id.getTimestamp() );
+// console.log( id.toHexString().length );
 
 // Create a new MongoClient
 const client = new MongoClient( connectionUri );
@@ -12,55 +18,19 @@ async function run() {
     // Use connect method to connect to the server
     await client.connect();
 
-    console.log('Connected successfully to server');
-
     const db = client.db( databaseName );
 
-    /*  db.collection('users').insertOne(
-        { name: "tusker", age: 30 }, (error,result)=>{
-            if( error ){
-                console.log("Unable to insert data");
-            }
-            console.log(`${result.insertedId} document was inserted`)
-        }); */
+    /* db.collection('users').insertOne({
+        '_id': id,
+        'name': 'tusker',
+        'age': 30
+    }, ( error, result ) => {
+        if( error ){
+            console.log( "Unable to insert data" );
+        }
 
-    /* const userList = [
-        { name: "tusker", age: 30 },
-        { name: "chloe", age: 26 },
-        { name: "theo", age: 24 }
-    ];
-
-    const options = { ordered: true };
-    db.collection('users').insertMany(
-        userList,
-        options, 
-        ( error, result ) => {
-            if( error ){
-                return console.log('Unable to insert user');
-            }
-
-            console.log(`${result.insertedCount} documents were inserted`);
-    });   */ 
-
-    // assignment
-    // insertmany to insert the documents
-    // setup the callbacks
-    // run the script
-    // refresh the database
-    const taskList = [
-        { description: "Learn Javascript", completed: true },
-        { description: "Learn react.js", completed: false },
-    ];
-
-    db.collection('tasks').insertMany(
-        taskList,
-        ( error, result ) => {
-            if( error ){
-                return console.log('Unable to insert user');
-            }
-
-            console.log(`${result.insertedCount} documents were inserted`);
-    });
+        console.log(`${result.insertedId} was inserted`);
+    }) */
 }
 
-run().catch( "Unable to insert data" );
+run();
