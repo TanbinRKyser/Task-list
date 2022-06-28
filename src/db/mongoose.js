cfcfb299b57b7f512466f4d27e9624fd7649d6a0
foxcatcher24 <tmitul515@gmail.com>
2022-06-28 20:53:33 +0600

    280622--validating & sanitizing data

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,31 +1,76 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-main().catch( err => console.log( err ) );
 
 async function main() {
     await mongoose.connect('mongodb://localhost:27017/task-manager-api');
 
     const userSchema = new mongoose.Schema({
         name: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         age: {
-            type: Number
+            type: Number,
+            default: 1,
+            validate( value ){
+                if( value < 0 ){
+                    throw new Error('Age must be a positive number');
+                }
+            }
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate( value ){
+                if( !validator.isEmail( value ) ){
+                    throw new Error('Not a valid email address');
+                }
+            }
         }
     });
     
     const User = mongoose.model('User', userSchema );
     
-    const Tusker = new User({
-        name: 'Tusker',
-        age: 30
+    const Pierre = new User({
+        name: ' Pierre',
+        age: 26,
+        email: 'Pierre1232@gmail.com'
     })
     
     
-    Tusker.save()
+    Pierre.save()
         .then( () =>{
-            console.log( Tusker );
+            console.log( Pierre );
         }).catch( ( error ) => {
             console.log(error);
         });
+
+    /* const taskSchema = new mongoose.Schema({
+        description: {
+            type: String
+        },
+        completed: {
+            type: Boolean
+        }
+    });
+
+    const Task = mongoose.model('Task', taskSchema );
+
+    const learnReact = new Task({
+        description: "Learn React.js",
+        completed: true
+    });
+
+    learnReact.save().then( ()=>{
+        console.log( learnReact );
+    }).catch( ( error ) => {
+        console.log( error );
+    }); */
+
 }
+
+main().catch( err => console.log( err ) );
