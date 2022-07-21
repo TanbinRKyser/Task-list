f3297d94d2e257936ec9799b173c8ba69189dd8a
foxcatcher24 <tmitul515@gmail.com>
2022-07-21 18:26:20 +0600

    210722--modified with async/await to simplifying the code

--- src/index.js
+++ src/index.js
@@ -8,81 +8,102 @@ const port = process.env.PORT || 3000;
 
 app.use( express.json() );
 
-/* app.post('/users',( request, response )=>{
+
+// Creating endpoints
+// --------------------------------------
+
+
+/* app.post('/users', async ( request, response ) => {
 
     const user = new User(request.body);
 
-    user.save().then(()=>{
-        response.send( user )
-    }).catch( ( error ) => {
-        response.status(400).send( error );
-    });
+    // user.save()
+    //     .then( () => {
+    //         response.send( user ) })
+    //     .catch( ( error ) => {
+    //         response.status( 400 ).send( error );
+    //     });
+
+        try{
+            await user.save();
+            response.status( 201 ).send( user );
+        } catch( error ){
+            response.status( 400 ).send( error );
+        }
 });
-*/
+ */
+
+/* app.get('/users', async ( request, response ) => {
+
+    try{
+        const users = await User.find({});
+        response.send( users );
+    } catch( error ){
+        response.status( 500 ).send( error );
+    }
+
+});
+
+app.get('/users/:id', async ( request, response ) => {
+    // console.log( request.params.id );
+    const _id = request.params.id;
+
+    try{
+        const user = await User.findById( _id );
+        if( !user ){
+            return response.status(404).send("User not found")
+        }
+        response.send( user );
+    } catch( error ){
+        response.status( 500 ).send( error );
+    }
+}); */
 
 
 // create a task model, endpoint and test
+// POST
 
-/* app.post('/tasks',( request, response )=>{
+app.post('/tasks', async ( request, response )=>{
 
     const task = new Task( request.body );
 
-    task.save().then(()=>{
-        response.status(201).send( task )
-    }).catch( ( error ) => {
+    try{
+        await task.save();
+        response.status(201).send( task );
+    } catch( error ){
         response.status(400).send( error );
-    });
-}); */
+    }
 
-    /* app.get('/users', ( request, response ) => {
-        User.find({
+});
 
-        }).then( ( users ) => {
-            response.send( users );
-        }).catch( ( error ) => {
-            response.status( 500 ).send( error );
-        })
-    }); 
+// GET
 
-    app.get('/users/:id', ( request, response ) => {
-        // console.log( request.params.id );
-        const _id = request.params.id;
+app.get('/tasks', async ( request, response ) => {
 
-        User.findById( _id )
-            .then( ( user ) => {
-                if( !user ){
-                    return response.status(404).send("User not found")
-                }
-                response.send( user );
-            }).catch( error => {
-                response.status( 500 ).send( error );
-            })
-    }); */
+    try{
+        const tasks = await Task.find( {} );
+        response.send( tasks );
+    } catch( error ){
+        response.status( 500 ).send( error );
+    }
+}); 
 
-    app.get('/tasks', ( request, response ) => {
-        Task.find({
+// GET id
+app.get('/tasks/:id', async ( request, response ) => {
 
-        }).then( ( tasks ) => {
-            response.send( tasks );
-        }).catch( ( error ) => {
-            response.status( 500 ).send( error );
-        })
-    }); 
+    const _id = request.params.id;
 
-    app.get('/tasks/:id', ( request, response ) => {
-        // console.log( request.params.id );
-        const _id = request.params.id;
+    try{
+        const task = await Task.findById( _id );
+        if( !task ){
+            return response.status(404).send("User not found");
+        }
+        response.send( task );
+    } catch( error ){
+        response.status( 500 ).send( error );
+    }
+});
 
-        Task.findById( _id )
-            .then( ( task ) => {
-                if( !task ){
-                    return response.status(404).send("User not found")
-                }
-                response.send( task );
-            }).catch( error => {
-                response.status( 500 ).send( error );
-            })
-    });
 
 app.listen( port, () => {
     console.log("Server is up on port.");
